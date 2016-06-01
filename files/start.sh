60bc614db47dfc7a1c87b733bdc48f3c49d598eb
Andrew R Kursar <akursar@gmail.com>
2016-06-01 14:40:26 -0400

    agent log to stdout

--- files/start.sh
+++ files/start.sh
@@ -5,19 +5,24 @@
 [ -z "${AGENT_UUID}" ] || echo -n "${AGENT_UUID}" > /var/lib/cloudera-scm-agent/uuid
 
 ## Assign config values from env, or use defaults
+AGENT_DIR=${AGENT_DIR:-/var/run/cloudera-scm-agent}
+LIB_DIR=${LIB_DIR:-/var/lib/cloudera-scm-agent}
+LISTENING_PORT=${PORT0:-9000}
+LOGDIR=${LOGDIR:-/var/log/cloudera-scm-agent}
+LOGFILE=${LOG_DIR:--}
+PACKAGE_DIR=${PACKAGE_DIR:-/usr/lib64/cmf/service}
+REPORTED_HOSTNAME=${REPORTED_HOSTNAME:-${HOST}}
 SERVER_HOST=${SERVER_HOST:-localhost}
 SERVER_PORT=${SERVER_PORT:-7182}
-LISTENING_PORT=${PORT0:-9000}
-REPORTED_HOSTNAME=${REPORTED_HOSTNAME:-${HOST}}
 SUPERVISORD_PORT=${PORT1:-19001}
 
 ## Write out config file
 cat > /etc/cloudera-scm-agent/config.ini << EOF
 [General]
-server_host=${SERVER_HOST}
-server_port=${SERVER_PORT}
 listening_port=${LISTENING_PORT}
 reported_hostname=${REPORTED_HOSTNAME}
+server_host=${SERVER_HOST}
+server_port=${SERVER_PORT}
 supervisord_port=${SUPERVISORD_PORT}
 EOF
 
@@ -32,6 +37,8 @@ EOF
 
 ## Finally, exec to cm-agent
 exec /usr/lib64/cmf/agent/build/env/bin/python /usr/lib64/cmf/agent/build/env/bin/cmf-agent \
-  --package_dir /usr/lib64/cmf/service \
-  --agent_dir /var/run/cloudera-scm-agent \
-  --lib_dir /var/lib/cloudera-scm-agent
+  --agent_dir=${AGENT_DIR} \
+  --lib_dir=${LIB_DIR} \
+  --logdir=${LOGDIR} \
+  --logfile=${LOGFILE} \
+  --package_dir=${PACKAGE_DIR}
